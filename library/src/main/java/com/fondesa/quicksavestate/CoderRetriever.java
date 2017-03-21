a69a86819157b70d5623db8f25deddaebfb1d5f0
antoniolig <giorgio.antonioli.dev@gmail.com>
2017-03-21 23:40:09 +0100

    javadoc StateCoder

--- library/src/main/java/com/fondesa/quicksavestate/CoderRetriever.java
+++ library/src/main/java/com/fondesa/quicksavestate/CoderRetriever.java
@@ -1,5 +1,22 @@
+/*
+ * Copyright (c) 2017 Fondesa
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.fondesa.quicksavestate;
 
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 
 import com.fondesa.quicksavestate.annotation.SaveState;
@@ -7,9 +24,23 @@ import com.fondesa.quicksavestate.coder.StateCoder;
 import com.fondesa.quicksavestate.exception.CoderNotFoundException;
 
 /**
- * Created by antoniolig on 01/03/17.
+ * Interface that manages the way in which a {@link StateCoder} is retrieved from an annotation.
+ * <br>
+ * The default implementation is: {@link DefaultCoderRetriever}.
+ * You can implement your own {@link CoderRetriever} and set it in your singleton instance
+ * with {@link QuickSaveState.Builder#coderRetriever(CoderRetriever)} (for example to
+ * optimize performance or to handle the cache differently).
  */
 public interface CoderRetriever {
+
+    /**
+     * Retrieve a {@link StateCoder} from an annotation and the annotated class.
+     *
+     * @param saveState           annotation obtained from the annotated field
+     * @param annotatedFieldClass java class of the annotate field
+     * @return not null coder used to serialize and deserialize the state {@link Bundle}
+     * @throws CoderNotFoundException if the coder can't be found or is unsupported
+     */
     @NonNull
     StateCoder getCoder(@NonNull SaveState saveState, @NonNull Class<?> annotatedFieldClass) throws CoderNotFoundException;
 }
